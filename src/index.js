3f328cf1e5c159fa20ccff8845bdeb394eedaa2b
Emily Erskine <s00209630@mail.itsligo.ie>
2024-06-23 18:12:33 +0100

    Editing so the footer is in the right spot

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Header } from './Common/Header/header';
 import './index.css';
-import { Footer } from './Common/Header/footer';
+import { Footer } from './Common/Footer/footer';
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"></link>
 
 ReactDOM.render(
