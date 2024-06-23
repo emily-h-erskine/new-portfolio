ef3de1580a266c6d96173c75d8c6718e34a794c6
Emily Erskine <s00209630@mail.itsligo.ie>
2024-06-23 18:06:40 +0100

    Simplifying when adding Project Data

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,50 +1,29 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import projectsData from '../projectData';
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
-        <div className="thumbnails recentworks row">
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <a className="thumbnail" href="/devilles">
+      <div className="thumbnails recentworks row">
+        {projectsData.map((project) => (
+          <div key={project.id} className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <a className="thumbnail" href={project.link}>
               <span className="img">
-                <img src="assets/images/devilles-home.png" alt="" />
+                <img src={project.imgSrc} alt="" />
                 <span className="cover"><span className="more">See details &rarr;</span></span>
               </span>
-              <span className="title">DeVille's Project</span>
+              <span className="title">{project.title}</span>
             </a>
-            <span className="details"><a href="/devilles">Web design</a> | <a href="/devilles-prototype">View Prototype</a></span>
+            <span className="details">
+              <a href={project.link}>{project.description}</a> | <a href={project.prototypeLink}>View Prototype</a>
+            </span>
             <h4></h4>
             <p></p>
           </div>
-
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <a className="thumbnail" href="/25">
-              <span className="img">
-                <img src="assets/images/25-home.png" alt="" />
-                <span className="cover"><span className="more">See details &rarr;</span></span>
-              </span>
-              <span className="title">Play 25 Online App Game</span>
-            </a>
-            <span className="details"><a href="/25">App Design</a> | <a href="/25-prototype">View Prototype</a></span>
-            <h4></h4>
-            <p></p>
-          </div>
-
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-            <a className="thumbnail" href="/RSA">
-              <span className="img">
-                <img src="assets/images/rsa-home.png" alt="" />
-                <span className="cover"><span className="more">See details &rarr;</span></span>
-              </span>
-              <span className="title">RSA Design Competition - Mail Call</span>
-            </a>
-            <span className="details"><a href="/RSA">App Design</a> | <a href="/RSA-prototype">View Prototype</a></span>
-            <h4></h4>
-            <p></p>
-          </div>
-        </div>
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/projectData.js
@@ -0,0 +1,29 @@
+const projectsData = [
+    {
+      id: 1,
+      title: "DeVille's Project",
+      imgSrc: "assets/images/devilles-home.png",
+      link: "/devilles",
+      description: "Web design",
+      prototypeLink: "/devilles-prototype",
+    },
+    {
+      id: 2,
+      title: "Play 25 Online App Game",
+      imgSrc: "assets/images/25-home.png",
+      link: "/25",
+      description: "App Design",
+      prototypeLink: "/25-prototype",
+    },
+    {
+      id: 3,
+      title: "RSA Design Competition - Mail Call",
+      imgSrc: "assets/images/rsa-home.png",
+      link: "/RSA",
+      description: "App Design",
+      prototypeLink: "/RSA-prototype",
+    },
+  ];
+  
+  export default projectsData;
+  
